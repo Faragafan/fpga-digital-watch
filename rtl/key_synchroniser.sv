0c3cbe2321a50f8d260709640af5d3da9ec3d0e0
Feifan <frankxia031225@gmail.com>
2026-05-11 13:49:36 +0000

    Key synchroniser done

--- rtl/key_synchroniser.sv
+++ rtl/key_synchroniser.sv
@@ -3,6 +3,13 @@
 module key_synchroniser (
     input logic clk,
     input logic [3:0] key_n,  // active -low , asynchronous
-    output logic [3:0] key_sync  // active -high , synchronised
+    output logic [3:0] key_sync = 'b0000  // active -high , synchronised
 );
+
+logic [3:0] key_stage1 = 'b0000;  // First stage of synchronisation
+
+always_ff @(posedge clk) begin
+    key_stage1 <= ~key_n;  // Invert and synchronise the key inputs
+    key_sync <= key_stage1;  // Second stage of synchronisation to prevent metastability
+end
 endmodule
